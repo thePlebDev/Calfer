e3676d95438be8d955db83afcafbc11074e8e3de
Tristan Elliott <>
2022-06-03 15:20:28 -0300

    initial commit

---
+++ src/main/java/com/calfer/calfer/CalferApplication.java
@@ -0,0 +1,13 @@
+package com.calfer.calfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calfer/calfer/CalferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calfer.calfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
